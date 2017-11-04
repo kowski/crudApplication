e553f8daaa0155446f6e3d1210cd6ee60cb3b399
Krystian <krystian.kalkowski@onet.pl>
2017-11-04 20:25:56 +0100

    Added university option and delete user option

---
+++ src/main/java/pl/lodz/uni/math/crudApplication/Student/UniversityJpaRepository.java
@@ -0,0 +1,7 @@
+package pl.lodz.uni.math.crudApplication.Student;
+
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+public interface UniversityJpaRepository  extends JpaRepository<University, Integer> {
+
+}
--- src/main/java/pl/lodz/uni/math/crudApplication/Student/UniversityRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.lodz.uni.math.crudApplication.Student;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UniversityRepository  extends CrudRepository<University, Long> {
-
-}
